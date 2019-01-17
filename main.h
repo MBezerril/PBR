85db6ae2e11b2a96fff8182bd3e5c3312442f197
MBezerril <moisesc.bezerril@gmail.com>
2019-01-17 15:20:17 -0300

    Perspective camera created

--- main.h
+++ main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+//Standard libs include
+#include <string>
 
+//Project files include
 #include "../Cameras/camera.h"
 #include "../Cameras/orthographic_camera.h"
+#include "../Cameras/perspective_camera.h"
 #include "../World/scene.h"
 #include "../Cameras/buffer.h"
 #include "../Rays/raytracer.h"
