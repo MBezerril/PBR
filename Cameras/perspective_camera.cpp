97f6f4e7341484954287e55c3a806b9136300c0b
MBezerril <moisesc.bezerril@gmail.com>
2019-01-16 23:14:50 -0300

    New file structure creted

---
+++ Cameras/perspective_camera.cpp
@@ -0,0 +1,38 @@
+#include "perspective_camera.h"
+
+PerspectiveCamera::PerspectiveCamera( void )
+{}
+ 
+PerspectiveCamera::PerspectiveCamera( const float min_x,
+                                        const float max_x,
+                                        const float min_y,
+                                        const float max_y,
+                                        const glm::ivec2 &resolution,
+                                        const glm::vec3 &position,
+                                        const glm::vec3 &up_vector,
+                                        const glm::vec3 &look_at,
+										const float depth_view_distance) :
+        Camera::Camera{ resolution, 
+                        position,
+                        up_vector,
+                        look_at },
+        min_x_{ min_x },
+        max_x_{ max_x },
+        min_y_{ min_y },
+        max_y_{ max_y },
+		depth_view_distance_{depth_view_distance}
+{}
+
+Ray PerspectiveCamera::getWorldSpaceRay( const glm::vec2 &pixel_coord ) const
+{
+    float width = max_x_ - min_x_;
+    float height = max_y_ - min_y_;
+
+    glm::vec3 origin{ pixel_coord[0] / static_cast< float >( resolution_[0] ) * width + min_x_,
+                      pixel_coord[1] / static_cast< float >( resolution_[1] ) * height + min_y_,
+                      0.0f };
+
+    return Ray{ onb_.getBasisMatrix() * origin + position_, 
+                glm::normalize( onb_.getBasisMatrix() * glm::vec3{ 0.0f, 0.0f, -1.0f } ) };
+}
+
--- main.h
+++ main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#include "camera.h"
-#include "orthographic_camera.h"
-#include "scene.h"
-#include "buffer.h"
-#include "raytracer.h"
+#include "../Cameras/camera.h"
+#include "../Cameras/orthographic_camera.h"
+#include "../World/scene.h"
+#include "../Cameras/buffer.h"
+#include "../Rays/raytracer.h"
 
 #endif /* MAIN_H_ */
 
