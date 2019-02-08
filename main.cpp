45e01f5c01ef57481996b8fe1ddf9f5e4cbba856
MBezerril <moisesc.bezerril@gmail.com>
2019-02-08 19:42:09 -0300

    Perspective camera created

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
+/*
+  Define the numbers of images to be rendered to create Gifs or animations
+*/
+#define imagesQuantity 1
+
 #include "main.h"
 
 int main(void) {
 	unsigned int x_resolution = 512;
 	unsigned int y_resolution = 512;
 
-	for (int actual = 0; actual < 10; actual++) {
-		std::string nome = "output_image" + std::to_string(actual) + ".ppm";
+	for (int actual = 0; actual < imagesQuantity; actual++) { //Loop used to make all images
+		std::string nome = "Outputs/output_image" + std::to_string(actual) + ".ppm";
 		glm::vec3 camera_position{ (float)actual/10.0, 0.0f,  1.0f };
 
-		PerspectiveCamera camera{ -1.25f,
+		OrthographicCamera camera{ -1.25f,
 									1.25f,
 								   -1.25f,
 									1.25f,
 									glm::ivec2{ x_resolution, y_resolution },
 									camera_position,     // position
 									glm::vec3{ 0.0f, 1.0f,  0.0f },     // up
-									glm::vec3{ 0.0f, 0.0f, -1.0f },   // look at
-									2.0f }; //Depthof field distance
+									glm::vec3{ 0.0f, 0.0f, -1.0f } // look at
+									};   
+									//2.0f }; //Depthof field distance for perspective Camera
 		Scene scene{};
 
 		scene.load();
